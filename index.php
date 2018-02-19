b54583cdd2494bddbf2c0f8429810b5bd5f68082
Derek McHugh <10355799@mydbs.ie>
2018-02-19 11:07:22 +0000

    fixed index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php
 echo "Hello World!";
 if(!empty($_GET['name'])){
-	echo "<BR>And a special shout out to ".$_GET['name']
+	echo "<BR>And a special shout out to ".$_GET['name'];
 }
 ?>
 </BODY>
